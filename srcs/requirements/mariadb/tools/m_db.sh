2d8a82cc9319757361b26f4b2c79ab615b534cf4
MTHAMIR <mohsinethamir@fedora.com>
2025-11-12 17:55:49 +0100

    la7ya lah ya3tik sa7a

--- srcs/requirements/mariadb/tools/m_db.sh
+++ srcs/requirements/mariadb/tools/m_db.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 
-# 
+# HERE IT WILL LISTEN ON LINUX SOCKET 
 service mariadb start
 sleep 10
 
 # 
-mariadb -e "create database if not exists $MYSQL_DATABASE;"
+mariadb -e "CREATE DATABASE IF NOT EXISTS  $MYSQL_DATABASE;"
 mariadb -e "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
-mariadb -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' identified by '$MYSQL_PASSWORD';"
+mariadb -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
 mariadb -e "FLUSH PRIVILEGES;"
 
-# 
+# HERE WILL SHUT IT DOWN AND RUN IT SAFELY IN A SPECIFIC PORT AND DEFAULT IP
 mariadb-admin shutdown
-mariadbd-safe --port=3306 --bind-address=0.0.0.0
+exec mariadbd-safe --port=3306 --bind-address=0.0.0.0
